03834e14b2863cdfd5a5cd4766e59bc3029bc407
M-Aitisam <aitisamahmed24@gmail.com>
2024-12-05 12:33:41 +0500

    update the code the code

---
+++ WebApp/wwwroot/calender.js
@@ -0,0 +1,22 @@
+﻿function initializeCalendar(events) {
+    var calendarEl = document.getElementById('calendar');
+
+    if (typeof FullCalendar === 'undefined') {
+        console.error('FullCalendar is not defined');
+        return;
+    }
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            start: 'prev,next today',
+            center: 'title',
+            end: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        events: events // Pass events from Blazor
+        
+
+    });
+
+    calendar.render();
+}
