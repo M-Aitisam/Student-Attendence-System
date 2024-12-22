b246506eda51114df8d4097fdab417b3cdcb6ce6
M-Aitisam <aitisamahmed24@gmail.com>
2024-12-23 02:50:38 +0500

    update teh code

---
+++ WebApp/wwwroot/theme.js
@@ -0,0 +1,14 @@
+﻿console.log("Theme script loaded");
+window.changeTheme = (theme) => {
+    console.log("Change theme function called");
+    if (theme === "Dark") {
+        document.body.style.backgroundColor = "#333";
+        document.body.style.color = "#fff";
+    } else if (theme === "Light") {
+        document.body.style.backgroundColor = "#fff";
+        document.body.style.color = "#000";
+    } else {
+        document.body.style.backgroundColor = "";
+        document.body.style.color = "";
+    }
+}
